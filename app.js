f93b642bc08bea477eb6eb63692b6ac537aa6305
raul <geana.raul@gmail.com>
2022-05-12 13:14:10 +0300

    some changes

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.get("/", function(req, res) {
 });
 
 //set app to listen on port 3000
-app.listen(3000, function() {
-    console.log("server is running on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+    console.log("server is running on port ", process.env.PORT || 3000);
 });
