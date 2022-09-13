0c6e2a8a1dcfbc6fe8e104b6c5474f8a56fcf104
RabaS <sebastian-stefan.raba@haufe.com>
2022-09-13 21:07:22 +0300

    chore(api): wire up repository

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+const todoRepository = require('./model/TodoRepository')
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 //render css files
@@ -14,32 +16,34 @@ var task = ["buy socks", "practise with nodejs"];
 var complete = ["finish jquery"];
 
 //post route for adding new task 
-app.post("/addtask", function(req, res) {
+app.post("/addtask", async function(req, res) {
     var newTask = req.body.newtask;
     //add the new task from the post route
-    task.push(newTask);
+    await todoRepository.create(newTask);
+
     res.redirect("/");
 });
 
-app.post("/removetask", function(req, res) {
-    var completeTask = req.body.check;
+app.post("/removetask", async function(req, res) {
+    var completeTaskId = req.body.check;
     //check for the "typeof" the different completed task, then add into the complete task
-    if (typeof completeTask === "string") {
-        complete.push(completeTask);
-        //check if the completed task already exits in the task when checked, then remove it
-        task.splice(task.indexOf(completeTask), 1);
-    } else if (typeof completeTask === "object") {
-        for (var i = 0; i < completeTask.length; i++) {
-            complete.push(completeTask[i]);
-            task.splice(task.indexOf(completeTask[i]), 1);
+    if (typeof completeTaskId === "string") {
+        await todoRepository.edit(completeTaskId, {completed: true})
+    } else if (typeof completeTaskId === "object") {
+        for (var i = 0; i < completeTaskId.length; i++) {
+            await todoRepository.edit(completeTaskId[i], {completed: true})
         }
     }
+    
     res.redirect("/");
 });
 
 //render the ejs and display added task, completed task
-app.get("/", function(req, res) {
-    res.render("index", { task: task, complete: complete });
+app.get("/", async function(req, res) {
+    const openTasks = await todoRepository.getAll(false);
+    const completeTasks = await todoRepository.getAll(true);
+
+    res.render("index", { task: openTasks, complete: completeTasks });
 });
 
 //set app to listen on port 3000
